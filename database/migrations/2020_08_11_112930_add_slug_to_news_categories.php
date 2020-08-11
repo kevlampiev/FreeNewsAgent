3a6811a9ff04ca35632b32bd3b47832e44ecf8de
Konstantin Evlampiev <kevlampiev@gmail.com>
2020-08-11 19:20:05 +0700

    new slug field in news_categories

---
+++ database/migrations/2020_08_11_112930_add_slug_to_news_categories.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class AddSlugToNewsCategories extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+            DB::statement('ALTER TABLE news_categories ADD COLUMN slug VARCHAR(50) AFTER id');
+            DB::statement('UPDATE news_categories SET slug=REPLACE(RIGHT(name,50)," ","_")');
+            DB::statement('ALTER TABLE articles.news_categories MODIFY COLUMN slug VARCHAR(50) UNIQUE NOT NULL');
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement('ALTER TABLE articles.news_categories DROP COLUMN slug');
+    }
+}
