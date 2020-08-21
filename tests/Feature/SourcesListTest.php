ef39b6e70cad20037261dc46e0157215ee22f3fe
Konstantin Evlampiev <kevlampiev@gmail.com>
2020-08-22 04:01:12 +0700

    some test renamed

--- tests/Feature/SourcesListTest.php
+++ tests/Feature/SourcesListTest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class CategoriesListTest extends TestCase
+class SourcesListTest extends TestCase
 {
     /**
      * A basic feature test example.
