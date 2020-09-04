d702e1016354398949a47f442b8397ae444b05d0
kevlampiev <kevlampiev@gmail.com>
2020-09-05 00:15:39 +0700

    added button fb

--- resources/views/layouts/mainlayout.blade.php
+++ resources/views/layouts/mainlayout.blade.php
@@ -96,7 +96,7 @@
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">
+                                <a class="nav-link" href="{{route('fbLogin')}}">
                                     <i class="fa fa-facebook" aria-hidden="true"></i>
                                 </a>
                             </li>
