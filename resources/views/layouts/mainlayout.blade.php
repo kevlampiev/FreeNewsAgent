e960e0786eab18e897c4c5663633224f5e2c27df
kevlampiev <kevlampiev@gmail.com>
2020-10-01 17:48:29 +0700

    made face a bit cleaner

--- resources/views/layouts/mainlayout.blade.php
+++ resources/views/layouts/mainlayout.blade.php
@@ -18,70 +18,53 @@
 
 </head>
 <body>
-<div class="container">
+<div class="container-fluid">
     <header>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <a class="navbar-brand" href="#">
                 <img src="{{asset('img/logo-customer.jpg')}}" class="main-logo-img">
             </a>
-            <button
-                class="navbar-toggler"
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbarNavDropdown"
-                aria-controls="navbarNavDropdown"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-            >
-                <span class="navbar-toggler-icon"></span>
-            </button>
+{{--            <button--}}
+{{--                class="navbar-toggler"--}}
+{{--                type="button"--}}
+{{--                data-toggle="collapse"--}}
+{{--                data-target="#navbarNavDropdown"--}}
+{{--                aria-controls="navbarNavDropdown"--}}
+{{--                aria-expanded="false"--}}
+{{--                aria-label="Toggle navigation"--}}
+{{--            >--}}
+{{--                <span class="navbar-toggler-icon"></span>--}}
+{{--            </button>--}}
+
+
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                    <li class="nav-item {{request()->routeIs('home')?'current-menu':''}}">
-                        <a class="nav-link" href="{{route('home')}}">Главная </a>
-                    </li>
-                    <li class="nav-item {{request()->routeIs('customer.categories')?'current-menu':''}}">
-                        <a class="nav-link" href="{{route('customer.categories')}}">Категории новостей</a>
-                    </li>
-
-                    <li class="nav-item dropdown {{(request()->routeIs('infoEnquieries.create')||request()->routeIs('customer.feedback'))?'current-menu':''}}">
-                        <a
-                            class="nav-link dropdown-toggle"
-                            href="{{route('customer.categories')}}"
-                            id="navbarDropdownMenuLink"
-                            data-toggle="dropdown"
-                            aria-haspopup="true"
-                            aria-expanded="false"
-                        >
-                            Запросы и обратная связь
-                        </a>
-                        <div
-                            class="dropdown-menu"
-                            aria-labelledby="navbarDropdownMenuLink"
-                        >
-                            <a @guest
-                               class="dropdown-item disabled"
-                               @else
-                               class="dropdown-item"
-                               @endguest
-                               href="{{route('customer.infoEnquiery')}}">Запрос информации</a>
-                            <a class="dropdown-item" href="{{route('customer.feedback')}}">Оставить отзыв о проекте</a>
-
-                        </div>
-                    </li>
-
-                    @if (Auth::user()&&Auth::user()->is_admin)
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{route('admin')}}">Панель администратора</a>
-                        </li>
-                    @endif
-
-
-                </ul>
 
                 <!-- Right Side Of Navbar -->
                 <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+
+                        <form class="form-inline searchform" method="POST" action="/search">
+                            @csrf
+                            <div class="input-group input-group-sm mb-3 border-white">
+                                <input
+                                    class="form-control bg-transparent"
+                                    type="search"
+                                    placeholder="Текст для поиска"
+                                    aria-label="Search"
+                                    name="searchStr"
+                                    aria-label="" aria-describedby="basic-addon1"
+                                />
+                                <div class="input-group-append">
+                                    <button class="btn btn-outline-secondary border-secondary" type="submit">
+                                        <i class="fa fa-search"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+
+                    </li>
                     <!-- Authentication Links -->
+
                     @guest
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('login') }}">Войти</a>
@@ -143,49 +126,78 @@
 
 
         </nav>
-        <div class="subheader">
-            <div>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item {{request()->routeIs('home')?'current-menu':''}}">
+                        <a class="nav-link" href="{{route('home')}}">Главная </a>
+                    </li>
+                    <li class="nav-item {{request()->routeIs('customer.categories')?'current-menu':''}}">
+                        <a class="nav-link" href="{{route('customer.categories')}}">Категории новостей</a>
+                    </li>
+
+                    <li class="nav-item dropdown {{(request()->routeIs('infoEnquieries.create')||request()->routeIs('customer.feedback'))?'current-menu':''}}">
+                        <a
+                            class="nav-link dropdown-toggle"
+                            href="{{route('customer.categories')}}"
+                            id="navbarDropdownMenuLink"
+                            data-toggle="dropdown"
+                            aria-haspopup="true"
+                            aria-expanded="false"
+                        >
+                            Запросы и обратная связь
+                        </a>
+                        <div
+                            class="dropdown-menu"
+                            aria-labelledby="navbarDropdownMenuLink"
+                        >
+                            <a @guest
+                               class="dropdown-item disabled"
+                               @else
+                               class="dropdown-item"
+                               @endguest
+                               href="{{route('customer.infoEnquiery')}}">Запрос информации</a>
+                            <a class="dropdown-item" href="{{route('customer.feedback')}}">Оставить отзыв о
+                                проекте</a>
+
+                        </div>
+                    </li>
+
+                    @if (Auth::user()&&Auth::user()->is_admin)
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{route('admin')}}">Панель администратора</a>
+                        </li>
+                    @endif
+
+
+                </ul>
+
 
             </div>
-
-            <form class="form-inline" method="POST" action="/search">
-                @csrf
-                <input
-                    class="form-control mr-sm-2"
-                    type="search"
-                    placeholder="Текст для поиска"
-                    aria-label="Search"
-                    name="searchStr"
-                    value=" "
-                />
-                <button class="btn btn-dark " type="submit">
-                    Поиск
-                </button>
-            </form>
-        </div>
+        </nav>
     </header>
 
     <div class="main">
 
-        <div class="left-nav">
+{{--        <div class="left-nav">--}}
 
-            <!-- Attention! Do not modify this code; END CODE -->
-            <div id="ml_5e40501f" style="margin-left: 15px;">
-                <div style="padding:0;margin: 0;" id="ml_5e40501f_i" v='1.1'
-                     a='{"t":"v","lang":"ru","ids":[],"a_dt_bg":"#757575","a_dt_c":"#FFFFFF","a_wn_bg":"#424242","a_wn_c":"#FFFFFF","a_wi_bg":"#757575","a_wi_c":"#FFFFFF"}'></div>
-                <div id="ml_5e40501f_c" style="padding:0;margin:0;padding:7px 5px;"><img
-                        src="https://meteolabs.ru/assets/img/logo_z_b.svg"
-                        style="width:15px;opacity:0.1;margin-right:5px;position:relative;top:1px;display:inline-block;"><a
-                        href="https://meteolabs.ru/погода_москва/сегодня/"
-                        style="color:#cccccc;font-size:12px;text-decoration:none;" target="_blank" id="ml_5e40501f_u">Погода
-                        сегодня</a></div>
-            </div>
-            <script async src="https://app.meteolabs.ru/js/?id=ml_5e40501f"></script>
+{{--            <!-- Attention! Do not modify this code; END CODE -->--}}
+{{--            <div id="ml_5e40501f" style="margin-left: 15px;">--}}
+{{--                <div style="padding:0;margin: 0;" id="ml_5e40501f_i" v='1.1'--}}
+{{--                     a='{"t":"v","lang":"ru","ids":[],"a_dt_bg":"#757575","a_dt_c":"#FFFFFF","a_wn_bg":"#424242","a_wn_c":"#FFFFFF","a_wi_bg":"#757575","a_wi_c":"#FFFFFF"}'></div>--}}
+{{--                <div id="ml_5e40501f_c" style="padding:0;margin:0;padding:7px 5px;"><img--}}
+{{--                        src="https://meteolabs.ru/assets/img/logo_z_b.svg"--}}
+{{--                        style="width:15px;opacity:0.1;margin-right:5px;position:relative;top:1px;display:inline-block;"><a--}}
+{{--                        href="https://meteolabs.ru/погода_москва/сегодня/"--}}
+{{--                        style="color:#cccccc;font-size:12px;text-decoration:none;" target="_blank" id="ml_5e40501f_u">Погода--}}
+{{--                        сегодня</a></div>--}}
+{{--            </div>--}}
+{{--            <script async src="https://app.meteolabs.ru/js/?id=ml_5e40501f"></script>--}}
 
-        </div>
+{{--        </div>--}}
 
 
-        <div class="content">
+{{--        <div class="content">--}}
             @if (session()->has('proceed_status'))
                 <div class="alert alert-success alert-dismissible fade show" role="alert">
                     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
@@ -205,7 +217,7 @@
             @endif
 
             @yield('content')
-        </div>
+{{--        </div>--}}
 
 
     </div>
